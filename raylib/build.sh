9fb7ae901e66823fd2542c2940f3eae6eaf642f7
Dylaris <3445818679@qq.com>
2025-08-29 22:27:24 +0800

    copy raylib library

--- raylib/build.sh
+++ raylib/build.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 set -xe
-g++ -Wall -Wextra -o main main.cc $(pkg-config raylib --cflags --libs)
-./main
+
+g++ -o main main.cc                               \
+    -Wall -Wextra -Wno-missing-field-initializers \
+    -ggdb                                         \
+    -I./raylib-5.5_linux_amd64/include            \
+    -L./raylib-5.5_linux_amd64/lib -lraylib       \
+    -Wl,-rpath=./raylib-5.5_linux_amd64/lib
